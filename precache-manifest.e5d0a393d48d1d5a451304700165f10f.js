71e23a645f8d2541869cda459c3d4f47e12f7384
Azure Pipelines <SpeedTechSolutions@visualstudio.com>
2020-07-11 05:34:47 +0000

    Release Version 2020.07.11.422

---
+++ precache-manifest.e5d0a393d48d1d5a451304700165f10f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800bb4074565fb3d7613c9452c566de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce8019a2fc6d54d3655f",
+    "url": "/static/css/main.179498af.chunk.css"
+  },
+  {
+    "revision": "6bb0bf91ec266c673f24",
+    "url": "/static/js/2.08bb1ad6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.08bb1ad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8019a2fc6d54d3655f",
+    "url": "/static/js/main.f02e029e.chunk.js"
+  },
+  {
+    "revision": "d7b66cb9dc8675324501",
+    "url": "/static/js/runtime-main.a324f94d.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
